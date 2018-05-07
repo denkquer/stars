fba4a05154546a835f67c867178d74d65c4e60fb
denkquer <36424493+denkquer@users.noreply.github.com>
2018-05-07 22:27:25 +0200

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,7 +3,8 @@ let stars = [];
 
 function setup() {
   let coin = createCanvas(innerWidth, innerHeight, WEBGL);
-  for (let i = 0; i < 2000; i++) {
+  for (let i = 0; i < 700; i++) {
+    7
     stars.push(new Star());
   }
   //true fullscreen! canvas only
@@ -19,8 +20,6 @@ function windowResized() {
 
 function draw() {
   background(0);
-
-  //translate(-width / 2, -height / 2, 0);
   camera(0, 0, 500 / tan(PI * 30.0 / 180.0), (width / 2 - mouseX) / 2, (height / 2 - mouseY) / 2, 0, 0, 1, 0);
 
   stars.forEach(function(Star) {
@@ -33,22 +32,27 @@ let Star = function() {
 
   this.x = random(-width * 2, width * 2);
   this.y = random(-height * 2, height * 2);
-  this.z = random(-300, 300);
-  this.speed = random(4, 9) / 3;
+  this.z = random(-3000, 3000);
+  this.speed = random(4, 90) / 3;
 
   this.show = function() {
     push();
     //translate(-width / 2, -height / 2, 0);
     translate(this.x, this.y, this.z);
     stroke(255);
-    sphere(this.z / 300);
+    sphere(this.z / 1500);
     pop();
   }
 
   this.update = function() {
+
     this.z += this.speed;
+
     if (this.z > 1000) {
-      this.z = -300;
+      this.z = -3000;
+      this.x = random(-width * 2, width * 2);
+      this.y = random(-height * 2, height * 2);
+      this.speed = random(4, 90) / 3;
     }
   }
 }
